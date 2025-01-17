052d5e1f8580555c5e49651a1519401b2625bde4
Brittany Halterman <halterma@gmail.com>
2025-01-16 20:12:02 -0500

    Handle Not Found errors

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,13 @@ import {
 } from 'react-router-dom';
 import './index.css';
 import Root from './routes/root';
+import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    // element: <div>Hello world!</div>,
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
