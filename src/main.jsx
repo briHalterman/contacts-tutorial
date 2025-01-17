7d24b4d7f22bbd3e1955af62511ee2ff72aa9a5a
Brittany Halterman <halterma@gmail.com>
2025-01-16 22:53:42 -0500

    Create index route

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import ErrorPage from './error-page';
 import Contact, { loader as contactLoader } from './routes/contact';
 import EditContact, { action as editAction } from './routes/edit';
 import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: 'contacts/:contactId',
         element: <Contact />,
